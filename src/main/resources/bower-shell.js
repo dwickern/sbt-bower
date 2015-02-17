e396371ae21eeabfd1b2f13ec3ed6c6454ed0dd6
Derek Wickern <dwickern@s4isystems.com>
2015-02-17 10:58:03 -0800

    Fix bower not downloading changed dependency versions
    Run 'bower update' instead of 'bower install'

--- src/main/resources/bower-shell.js
+++ src/main/resources/bower-shell.js
@@ -10,7 +10,7 @@ process.chdir(cwd);
 
 var bower = require('bower');
 
-bower.commands.install(null, null, { cwd: cwd, directory: target })
+bower.commands.update(null, null, { cwd: cwd, directory: target })
     .on('error', function(error) {
         console.log("\u0010" + JSON.stringify({ success: false, error: error.message }));
     })
